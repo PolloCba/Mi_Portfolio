b21977463ce7fc227a6028c2279e7cdd9e9d75d3
AmayaClaudio84 <amayaclaudio84@gmail.com>
2022-09-15 15:45:27 -0300

    Boton Disabled formulario de contacto

--- habilitar_boton.js
+++ habilitar_boton.js
@@ -1,19 +1,19 @@
-function habilitar(){
-    const text = document.getElementById("textarea");
-    let valor = 0;
+function habilitar() {
+  const text = document.getElementById("textarea");
+  let valor = 0;
 
-    if (text == ""){
-        valor++;
-    }
+  if (text == "") {
+    valor++;
+  }
 
-    if(valor == 0){
-        document.getElementById("boton").disabled = false;
-    }else{
-        document.getElementById("boton").disabled = true;
-    }
+  if (valor == 0) {
+    document.getElementById("button").disabled = false;
+  } else {
+    document.getElementById("button").disabled = true;
+  }
 }
 
 document.getElementById("textarea").addEventListener("keyup", habilitar);
-document.getElementById("boton").addEventListener("click", ()=>{
-    console.log("Haz completado el formulario");
-})
+document.getElementById("button").addEventListener("click", () => {
+  console.log("Haz completado el formulario");
+});
